118af97e32672293cb3673ad05370dcc33da2b22
lsj324 <dltjswo324@naver.com>
2024-06-21 11:17:37 +0900

    Update Arduino.ino

--- 로봇암 제어/Arduino.ino
+++ 로봇암 제어/Arduino.ino
@@ -1,43 +1,36 @@
+#include <Servo.h>
+
+Servo base_servo;
+Servo shoulder_servo;
+Servo elbow_servo;
+Servo wrist_servo;
+
 void setup() {
-  // 아두이노 셋업 함수, 한 번만 실행됨
   Serial.begin(9600); // 시리얼 통신을 9600bps로 시작
 
-  // 데이터 문자열 초기화
-  String data = "a10b20c30d40\n";
-
-  // 'b', 'c', 'd'의 인덱스 찾기
-  int index_b = data.indexOf('b');
-  int index_c = data.indexOf('c');
-  int index_d = data.indexOf('d');
-
-  // 'b'의 인덱스 출력
-  Serial.println(index_b);
-
-  // 'a'와 'b' 사이의 값을 정수로 변환하여 base_ang에 저장
-  int base_ang = data.substring(1, index_b).toInt();
-  Serial.println(base_ang); // base_ang 값 출력
-
-  // 'b'와 'c' 사이의 값을 정수로 변환하여 shoulder_ang에 저장
-  int shoulder_ang = data.substring(index_b + 1, index_c).toInt();
-
-  // 'c'와 'd' 사이의 값을 정수로 변환하여 elbow_ang에 저장
-  int elbow_ang = data.substring(index_c + 1, index_d).toInt();
-
-  // 'd' 이후의 값을 정수로 변환하여 wrist_ang에 저장
-  int wrist_ang = data.substring(index_d + 1).toInt();
-
-  // shoulder_ang, elbow_ang, wrist_ang 값 출력
-  Serial.println(shoulder_ang);
-  Serial.println(elbow_ang);
-  Serial.println(wrist_ang);
+  // 서보 모터 초기화
+  base_servo.attach(9);     // 베이스 서보 핀 (9번 핀에 연결)
+  shoulder_servo.attach(10); // 숄더 서보 핀 (10번 핀에 연결)
+  elbow_servo.attach(11);    // 엘보 서보 핀 (11번 핀에 연결)
+  wrist_servo.attach(12);    // 손목 서보 핀 (12번 핀에 연결)
 }
 
 void loop() {
-  // 루프 함수, 계속 반복 실행됨
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    int first_index = data.indexOf(' ');
+    int second_index = data.indexOf(' ', first_index + 1);
+    int third_index = data.indexOf(' ', second_index + 1);
 
-  // 시리얼 데이터가 있을 때 실행
-  // if (Serial.available() > 0) {
-  //   String data = Serial.readStringUntil('\n'); // '\n'까지 문자열 읽기
-  //   int index_b = data.indexOf('b'); // 'b'의 인덱스 찾기
-  // }
+    int base_ang = data.substring(0, first_index).toInt();
+    int shoulder_ang = data.substring(first_index + 1, second_space).toInt();
+    int elbow_ang = data.substring(second_index + 1, third_space).toInt();
+    int wrist_ang = data.substring(third_index + 1).toInt();
+
+    // 서보 모터 각도 설정
+    base_servo.write(base_ang);
+    shoulder_servo.write(shoulder_ang);
+    elbow_servo.write(elbow_ang);
+    wrist_servo.write(wrist_ang);
+  }
 }
